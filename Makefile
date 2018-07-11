fbdcb0a3ee5da309f4413cbad0773858cadc5536
cx <cx@cx-good.(none)>
2018-07-11 15:34:09 +0800

    add my codes

---
+++ Makefile
@@ -0,0 +1,31 @@
+# Makefile for the socket programming example
+#
+
+simple_server_objects = ServerSocket.o Socket.o servermain.o
+simple_client_objects = ClientSocket.o Socket.o clientmain.o
+
+GPP = arm-none-linux-gnueabi-g++
+
+
+all : simple_server simple_client
+
+simple_server: $(simple_server_objects)
+	$(GPP) -o simple_server $(simple_server_objects)
+
+
+simple_client: $(simple_client_objects)
+	$(GPP) -o simple_client $(simple_client_objects)
+
+%.o : %.cpp
+	$(GPP) -c $^ -o $@
+
+
+#Socket: Socket.cpp
+#ServerSocket: ServerSocket.cpp
+#ClientSocket: ClientSocket.cpp
+#servermain: servermain.cpp
+#clientmain: clientmain.cpp
+
+
+clean:
+	rm -f *.o simple_server simple_client
---
+++ clientmain.cpp
@@ -0,0 +1,45 @@
+#include "ClientSocket.h"
+#include "SocketException.h"
+#include <iostream>
+#include <string>
+
+int main ( int argc, int argv[] )
+{
+
+  std::string strIp = "";
+  std::string strPort = "";
+  try
+    {
+      std::cout << "please input ip to connect:\n"
+      std::cin >> strIp;
+      std::cout << "please input port to connect\n"
+      std::cin >> strPort;
+      std::cout << "connecting Ip: " << strIp << " Port: " << strport;
+
+
+      ClientSocket client_socket ( "localhost", 30000 );
+
+      std::string reply;
+      std::string getStr;
+      while(true)
+      {
+          try
+	  {
+              std::cin >> getStr;
+	      client_socket << getStr;
+	      client_socket >> reply;
+	  }
+          catch ( SocketException& ) {}
+
+          std::cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+      }
+
+    }
+  catch ( SocketException& e )
+    {
+      std::cout << "Exception was caught:" << e.description() << "\n";
+    }
+
+  return 0;
+}
+
